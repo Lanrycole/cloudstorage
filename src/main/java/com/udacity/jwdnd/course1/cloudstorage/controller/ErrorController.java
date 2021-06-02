83d7e245f520234dac6603e8f71bdf605077b2d0
lanreycole <lanre1012@yahoo.com>
2021-06-01 22:34:19 -0400

    3 Test cases implemented

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ErrorController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ErrorController.java
@@ -1,21 +1,41 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ErrorController  implements org.springframework.boot.web.servlet.error.ErrorController {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
-    @RequestMapping("/error")
-    public String handleError()
-    {
+@Controller
+@RequestMapping("/error")
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-        return "<h3>Page Not Found return <a href='/home'>Home</a></h3>";
+    /**
+     * This method routes errors to the appropriate error page, defaulting to the plain error page if none found
+     *
+     * @param request HttpServletRequest to process
+     * @return error page to be displayed
+     */
+    @GetMapping()
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int httpStatusCode = Integer.parseInt(status.toString());
+            if (httpStatusCode == HttpStatus.NOT_FOUND.value()) {
+                return "ErrorPages/error-404";
+            } else if (httpStatusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "ErrorPages/error-505";
+            }
+        }
+        return "error-page";
     }
 
     @Override
     public String getErrorPath() {
-        return "/error";
+        return null;
     }
 }
